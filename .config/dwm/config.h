74d7fc926f54804479c72921bfd4d3a796c40902
kael <kangxiaoju655@gmail.com>
2022-05-19 19:13:23 +0800

    删除wezterm配置，添加alacritty配置

--- .config/dwm/config.h
+++ .config/dwm/config.h
@@ -20,8 +20,8 @@ static const int smartgaps          = 0;        /* 1 means no outer gap when the
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const Bool viewontag         = True;     /* Switch view on tag switch */
-static const char *fonts[]          = { "JetBrainsMono Nerd Font:style:medium:size=10",
-                                        "Material Design Icons-Regular:size=10",
+static const char *fonts[]          = { "JetBrainsMono Nerd Font:style:medium:size=8",
+                                        "Material Design Icons-Regular:size=8",
                                       };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#1e222a";
@@ -92,7 +92,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL }; 
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
 static const char *firefox[]  = { "firefox", NULL };
 
 static const char *upvol[]   = { "/home/kael/scripts/vol-up.sh",  NULL };
@@ -102,7 +102,7 @@ static const char *mutevol[] = { "/home/kael/scripts/vol-toggle.sh",  NULL };
 static const char *wpcmd[]  = { "/home/kael/scripts/wp-change.sh", NULL };
 static const char *sktogglecmd[]  = { "/home/kael/scripts/sck-tog.sh", NULL };
 static const char scratchpadname[] = "scratchpad";
-static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x40", NULL };
+static const char *scratchpadcmd[] = { "alacritty", "-t", scratchpadname, "--config-file", "/home/kael/.config/alacritty/scratchpad_alacritty.yml", NULL };
 
 static const char *setcolemakcmd[]  = { "/home/kael/scripts/setxmodmap-colemak.sh", NULL };
 
